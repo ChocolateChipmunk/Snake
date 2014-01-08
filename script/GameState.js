c91b51e72fb4ffc47e7709e052d61c6d479da916
ChocolateChipmunk <jfowler_80@hotmail.com>
2014-01-08 09:41:02 -0500

    Create GameState.js

---
+++ script/GameState.js
@@ -0,0 +1,66 @@
+/*
+* The GameState class is responsible for maintaining the current layout of the game board, including the location
+* of any existing game elements (characters, food, etc.) The class contains public methods that allow elements to
+* be added or moved.
+*/
+
+function GameState (rowCount, colCount){
+    this.rowCount = rowCount;
+    this.colCount = colCount;
+    this.state = new Array();
+
+    for(var row = 0; row < rowCount; row++){
+        var rowArray = new Array();
+
+        for(var col = 0; col < colCount; col++){
+            rowArray.push(0);
+        }
+
+        this.state.push(rowArray);
+    }
+}
+
+GameState.prototype.clear = function(){
+    for(var row = 0; row < this.rowCount; row++){
+        for(var col = 0; col < this.colCount; col++){
+            this.state[row][col] = 0;
+        }
+    }
+}
+
+GameState.prototype.removeSnake = function (snake) {
+    for (var i = 0; i < snake.length; i++) {
+        this.state[snake.body[i].row][snake.body[i].col] = 0;
+    }
+}
+
+GameState.prototype.addSnake = function (snake) {
+    for (var i = 0; i < snake.length; i++) {
+        if (i == 0) {
+            this.state[snake.body[i].row][snake.body[i].col] = 3;
+        }
+        else {
+            this.state[snake.body[i].row][snake.body[i].col] = 1;
+        }
+    }
+}
+
+GameState.prototype.addFood = function (row, col) {
+    this.state[row][col] = 2;
+}
+
+GameState.prototype.removeFood = function (row, col) {
+    this.state[row][col] = 0;
+}
+
+GameState.prototype.spaceContainsSnakeBody = function (row, col){
+    return this.state[row][col] === 1;
+}
+
+GameState.prototype.spaceContainsSnakeHead = function (row, col){
+    return this.state[row][col] === 3;
+}
+
+GameState.prototype.spaceContainsFood = function (row, col){
+    return this.state[row][col] === 2;
+}
